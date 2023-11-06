b6f3528a9448fd119c47be37eaf4a78c8d96f670
avinash <avinashshiyani760@example.com>
2023-11-06 16:15:26 +0530

    completted edgeCut

--- edgeCut/assets/js/script.js
+++ edgeCut/assets/js/script.js
@@ -1,24 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+  },
+  navigation: {
+    nextEl: ".right-swipe",
+    prevEl: ".left-swipe",
+  },
+  breakpoints: {
+    640: {
+      spaceBetween: 120,
     },
-    autoplay: {
-        delay: 4000,
-      },
-    navigation: {
-        nextEl: ".right-swipe",
-        prevEl: ".left-swipe",
+    1024: {
+      spaceBetween: 200,
     },
-    breakpoints: {
-        640:{
-            spaceBetween:120,
-        },
-        1024:{
-            spaceBetween:200
-        }
-    }
+  },
+});
+
+// hamburger code
+
+let hamBurger = document.querySelector(".hamburger");
+let frontHamburger = document.querySelector(".fronthamburger");
+hamBurger.addEventListener("click", () => {
+  hamBurger.classList.toggle('rotateham');
+  frontHamburger.classList.toggle("bringmenu");
 });
